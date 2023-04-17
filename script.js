a11847b551b3739219a00ba9bdeac8e8799bfc02
merveeolcay <130758491+merveeolcay@users.noreply.github.com>
2023-04-17 23:13:14 +0300

    added addTodo logic

--- script.js
+++ script.js
@@ -5,4 +5,29 @@ const todoInput = document.querySelector(".todo-add"),
   completeTodo = document.querySelector(".todo-complete"),
   deleteTodo = document.querySelector(".todo-delete"),
   searchInput = document.querySelector(".todo-search"),
-  deleteCompletedBtn = document.querySelector(".todo-completed-btn");
+  deleteCompletedBtn = document.querySelector(".todo-completed-btn"),
+  todoForm = document.querySelector(".todo-form"),
+  todosContainer = document.querySelector(".todo-list");
+
+function addTodo(event) {
+  event.preventDefault();
+  let todoValue = todoInput.value;
+  let todoHtml = `
+    <li class="todo-wrapper">
+    <div class="todo-text">${todoValue}</div>
+    <div class="todo-icons">
+      <div class="todo-complete">
+        <i class="fa-regular fa-square-check"></i>
+      </div>
+      <div class="todo-delete">
+        <i class="fa-regular fa-trash-can"></i>
+      </div>
+    </div>
+  </li>`;
+  todosContainer.insertAdjacentHTML("afterend", todoHtml); 
+  
+
+  todoInput.value = "";
+}
+
+todoForm.addEventListener("submit", addTodo);
