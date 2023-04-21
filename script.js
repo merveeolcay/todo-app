ce62af44027c70efb9a51d9ccb211c8f72f360a0
merveeolcay <130758491+merveeolcay@users.noreply.github.com>
2023-04-21 18:43:02 +0300

    added value validation
    1) if there is value add
    2) trim value

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function addTodo(event) {
   let todoValue = todoInput.value;
   let todoHtml = `
     <li class="todo-wrapper">
-    <div class="todo-text">${todoValue}</div>
+    <div class="todo-text">${todoValue.trim()}</div>
     <div class="todo-icons">
       <div onclick="checkTodo(event)" class="todo-complete">
         <i class="fa-regular fa-square-check"></i>
@@ -31,7 +31,9 @@ function addTodo(event) {
     </div>
   </li>`;
 
-  todosContainer.insertAdjacentHTML("beforeend", todoHtml);
+  if (todoValue) {
+    todosContainer.insertAdjacentHTML("beforeend", todoHtml);
+  }
   todoInput.value = "";
 }
 
@@ -48,7 +50,8 @@ function removeTodo(event) {
 }
 
 function checkTodo(event) {
-  const todoText = event.target.parentElement.parentElement.previousElementSibling;
+  const todoText =
+    event.target.parentElement.parentElement.previousElementSibling;
   todoText.classList.toggle("line-through");
 }
 
@@ -66,13 +69,12 @@ function searchTodo(event) {
   }
 }
 
-function deleteCheckedTodos(event){
-  
+function deleteCheckedTodos(event) {
   const todos = document.querySelectorAll(".todo-wrapper");
   for (let i = 0; i < todos.length; i++) {
-  const checkedTodos = todos[i].querySelector(".todo-text.line-through");
-  if(checkedTodos){
-    todos[i].remove();
-  }
+    const checkedTodos = todos[i].querySelector(".todo-text.line-through");
+    if (checkedTodos) {
+      todos[i].remove();
+    }
   }
 }
